dc59999a8f945cad813366d15d5e9f845401fef4
Adeyi Gbeminiyi <adeyigbeminiyi414@gmail.com>
2026-02-10 22:53:22 +0100

    Add QuizPage component to display quiz ID
    Signed-off-by: Adeyi Gbeminiyi  <adeyigbeminiyi414@gmail.com>

--- src/pages/quiz/[quizId].jsx
+++ src/pages/quiz/[quizId].jsx
@@ -1,1 +1,8 @@
+import { useRouter } from "next/router";
 
+export default function QuizPage() {
+  const router = useRouter();
+  const { quizId } = router.query;
+
+  return <h1>Quiz ID: {quizId}</h1>;
+}
