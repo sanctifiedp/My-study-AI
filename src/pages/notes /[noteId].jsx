61133c427fb7b7ec9d28ac918227f5611cc09d9a
Adeyi Gbeminiyi <adeyigbeminiyi414@gmail.com>
2026-02-10 22:51:44 +0100

    Add NoteDetail component to display note ID
    Signed-off-by: Adeyi Gbeminiyi  <adeyigbeminiyi414@gmail.com>

--- src/pages/notes /[noteId].jsx
+++ src/pages/notes /[noteId].jsx
@@ -1,1 +1,8 @@
+import { useRouter } from "next/router";
 
+export default function NoteDetail() {
+  const router = useRouter();
+  const { noteId } = router.query;
+
+  return <h1>Note ID: {noteId}</h1>;
+}
