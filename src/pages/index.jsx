0e30389623c7a63e1774229db047ec1db50f107d
Adeyi Gbeminiyi <adeyigbeminiyi414@gmail.com>
2026-02-10 21:56:31 +0100

    Add first working page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,1 +1,14 @@
-
+export default function Home() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      background: "white",
+      fontFamily: "Arial"
+    }}>
+      <h1>Hello 👋 The app is alive</h1>
+    </div>
+  );
+}
